3bca44c4921a0943a39a24cc094da520cd4d3d1b
“Camila <camilacarvalho@sou.faccat.br>
2020-06-22 15:15:19 -0300

    Aqui foi inserido a classe partida de xadrez e iniciado a implementação do método realizar jogada, também foi delegada para a Partida de Xadrez instanciar as peças (não fica mais no programa principal)

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -41,6 +41,19 @@ namespace tabuleiro
             pecas[pos.linha, pos.coluna] = p; //aqui informa  a posição que a "variavel" p vai permanecer
             p.posicao = pos; //a "variável" p vai receber como posição a matriz(linha e coluna) indicada pelo usuario
         }
+        //metodo para retirar a peça da posição
+        public Peca retirarPeca(Posicao pos)
+        {
+            if(peca(pos) == null) //se não tiver nenhuma peça na posição retorna nula
+            {
+                return null;
+            }
+            Peca aux = peca(pos);//se tiver uma peça, necessário criar uma variável auxiliar e atribuir este valor que tinha nesta posição a esta variável
+            aux.posicao = null; //depois de atribuir o valor a variável, é necessário informar que esta peça não está mais no tabuleiro, ou seja, ela é nula
+            pecas[pos.linha, pos.coluna] = null; //depois de "salvar" o valor que tinha no campo informado, é necessário deixar o campo no tabuleiro nulo, para ele poder receber outra peça
+            return aux; //por fim, retorna a auxiliar, pra poder colocar ela em outro local
+        }
+
 
         //método para validar se uma posição é válida
         public bool posicaoValida(Posicao pos)
---
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PartidaDeXadrez
+    {
+        public Tabuleiro tab { get; private set; }//para uma partida de xadrez é necessário o tabuleiro (que ja possui as peças)
+        private int turno;//necessário o turno
+        private Cor jogadorAtual;//necessário saber o jogador atual
+
+        //construtor com parametros
+        public PartidaDeXadrez()
+        {
+            tab = new Tabuleiro(8, 8);//aqui já está instanciando o tabuleiro e informando o tamanho dele
+            turno = 1; //aqui que inicia no primeiro turno
+            jogadorAtual = Cor.Branca; //todos os jogos de xadrez inicia com a peça branca
+            colocarPecas();
+        }
+
+        //metodo para executar os movimentos da peça
+        public void executaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tab.retirarPeca(origem);
+            p.incrementarQteMovimentos();
+            Peca pecaCapturada = tab.retirarPeca(destino);
+            tab.colocarPeca(p, destino);
+        }
+
+        private void colocarPecas()
+        {
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 1).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 2).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('d', 2).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('e', 2).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('e', 1).toPosicao());
+            tab.colocarPeca(new Rei(tab, Cor.Branca), new PosicaoXadrez('d', 1).toPosicao());
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 7).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 8).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('d', 7).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 7).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 8).toPosicao());
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new PosicaoXadrez('d', 8).toPosicao());
+        }
+
+    }
+}
