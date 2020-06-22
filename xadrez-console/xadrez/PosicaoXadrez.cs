17676269a46f41cdfee1f6618c6c79f733942eb5
“Camila <camilacarvalho@sou.faccat.br>
2020-06-22 13:47:50 -0300

    Criado a classe posição xadrez, pois é necessário realizar uma conversão da matriz para o modo em que um tabuleiro é interpretado

---
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using tabuleiro;
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        //função para converter a posição do xadrez real para a matriz, pois a posição do xadrez 8,a é correspondente a 0,0 na matriz
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + linha + coluna;
+        }
+    }
+}
