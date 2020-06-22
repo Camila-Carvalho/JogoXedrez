ba014b2e1d2ffacbbedb7b743e8e921d8def1fd8
“Camila <camilacarvalho@sou.faccat.br>
2020-06-22 11:04:22 -0300

    Até aqui foi criado o tabuleiro somente, está imprimindo somente o tabuleiro

---
+++ xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    enum Cor
+    {//como éuma classe cor, para que fosse reaproveitada em vários lugares além das peças, poderia colocar várias cores
+        Branca,
+        Preta,
+    }
+}
