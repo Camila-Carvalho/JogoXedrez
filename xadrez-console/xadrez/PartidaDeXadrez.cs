6ee791302eb7957d56b87770acc10a9c6009478d
“Camila <camilacarvalho@sou.faccat.br>
2020-06-23 16:19:28 -0300

    Adicionado a captura de peças

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using tabuleiro;
 
 namespace xadrez
@@ -9,6 +9,8 @@ namespace xadrez
         public int turno { get; private set; }//necessário o turno
         public Cor jogadorAtual { get; private set; }//necessário saber o jogador atual
         public bool terminada { get; private set; }
+        private HashSet<Peca> pecas;//aqui é uma variável do tipo CONJUNTO de PEÇAS
+        private HashSet<Peca> capturadas;
 
         //construtor com parametros
         public PartidaDeXadrez()
@@ -17,6 +19,8 @@ namespace xadrez
             turno = 1; //aqui que inicia no primeiro turno
             jogadorAtual = Cor.Branca; //todos os jogos de xadrez inicia com a peça branca
             terminada = false;
+            pecas = new HashSet<Peca>();//necessário declarar o conjunto antes de colocar as peças
+            capturadas = new HashSet<Peca>();
             colocarPecas();
         }
 
@@ -27,6 +31,10 @@ namespace xadrez
             p.incrementarQteMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
+            if(pecaCapturada != null)//se tiver peça na posição
+            {
+                capturadas.Add(pecaCapturada);//adiciona ela no conjunto de peças capturas, independente da cor
+            }
         }
 
         public void realizaJogada(Posicao origem, Posicao destino)
@@ -70,22 +78,56 @@ namespace xadrez
                 jogadorAtual = Cor.Branca;
             }
         }
+        //separar as peças capturadas apenas cor que for informada
+        public HashSet<Peca> pecasCapturadas(Cor cor)
+        {
+            HashSet<Peca> aux = new HashSet<Peca>();
+            foreach (Peca x in capturadas)
+            {
+                if(x.cor == cor)
+                {
+                    aux.Add(x);
+                }
+            }
+            return aux;
+        }
+
+        public HashSet<Peca> pecasEmJogo(Cor cor)
+        {
+            HashSet<Peca> aux = new HashSet<Peca>();
+            foreach (Peca x in pecas)
+            {
+                if (x.cor == cor)
+                {
+                    aux.Add(x);
+                }
+            }
+            aux.ExceptWith(pecasCapturadas(cor));//aqui é para retirar do conjunto todas as peças capturadas da cor informada, assim, da pra saber quais as peças que ainda estão em jogo
+            return aux;
+        }
+
+        public void colocarNovaPeca(char coluna, int linha, Peca peca)
+        {
+            tab.colocarPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());//no tabuleiro da partida deve-se colocar a peça
+            pecas.Add(peca);//no conjunto peças da partida, deve-se adicionar uma peça
+        }
+
 
         private void colocarPecas()
         {
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 1).toPosicao());
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 2).toPosicao());
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('d', 2).toPosicao());
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('e', 2).toPosicao());
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('e', 1).toPosicao());
-            tab.colocarPeca(new Rei(tab, Cor.Branca), new PosicaoXadrez('d', 1).toPosicao());
+            colocarNovaPeca('c', 1, new Torre(tab, Cor.Branca));
+            colocarNovaPeca('c', 2, new Torre(tab, Cor.Branca));
+            colocarNovaPeca('d', 2, new Torre(tab, Cor.Branca));
+            colocarNovaPeca('e', 2, new Torre(tab, Cor.Branca));
+            colocarNovaPeca('e', 1, new Torre(tab, Cor.Branca));
+            colocarNovaPeca('d', 1, new Rei(tab, Cor.Branca));
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 7).toPosicao());
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 8).toPosicao());
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('d', 7).toPosicao());
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 7).toPosicao());
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 8).toPosicao());
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new PosicaoXadrez('d', 8).toPosicao());
+            colocarNovaPeca('c', 7, new Torre(tab, Cor.Preta));
+            colocarNovaPeca('c', 8, new Torre(tab, Cor.Preta));
+            colocarNovaPeca('d', 7, new Torre(tab, Cor.Preta));
+            colocarNovaPeca('e', 7, new Torre(tab, Cor.Preta));
+            colocarNovaPeca('e', 8, new Torre(tab, Cor.Preta));
+            colocarNovaPeca('d', 8, new Rei(tab, Cor.Preta));
         }
 
     }
--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -13,26 +13,26 @@ namespace xadrez
         {
             return "R";
         }
-
+        //metodo auxiliar pra ver se a posição de destino está livre ou se ela é ocupada por uma peça adversaria
         private bool podeMover(Posicao pos)
         {
             Peca p = tabuleiro.peca(pos);
             return p == null || p.cor != cor;
         }
-
+        //implementação dos movimentos possíveis
         public override bool[,] movimentosPossiveis()
         {
-            bool[,] mat = new bool[tabuleiro.linhas, tabuleiro.colunas];
+            bool[,] mat = new bool[tabuleiro.linhas, tabuleiro.colunas];//cria uma nova matriz para apresentar as posições possiveis
 
-            Posicao pos = new Posicao(0, 0);
+            Posicao pos = new Posicao(0, 0);//aqui é somente para instanciar e iniciar com algo para os testes
 
             // acima
-            pos.definirValores(posicao.linha - 1, posicao.coluna);
-            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
+            pos.definirValores(posicao.linha - 1, posicao.coluna);//aqui é pra pegar a posição da peça de acordo com a função definir valores da classe posiçao
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))//aqui é para testar se a posição que foi pega anteriormente é válida e se pode mover
             {
                 mat[pos.linha, pos.coluna] = true;
             }
-            // ne
+            // nordeste
             pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
             if (tabuleiro.posicaoValida(pos) && podeMover(pos))
             {
